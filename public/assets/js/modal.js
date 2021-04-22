873088f3c24853c98ad40e7068f3edd372a620f0
Anthony Alves <contact@anthonyalves.fr>
2021-04-22 17:15:24 +0200

    first commit message.

---
+++ public/assets/js/modal.js
@@ -0,0 +1,24 @@
+// Affichage du modal de suppression dans l'index
+
+var nom;
+var id;
+
+function openIndexModal(nom, id) {
+  nom = nom;
+  id = id;
+  var x = document.getElementById("modal");
+  document.documentElement.style.overflow = "hidden";
+  x.style.display = "block";
+  afficherNom(nom);
+  document.getElementById("modal-delect-trick").href ="tricks/detail/" + id + "/delect";
+}
+
+function closeIndexModal() {
+  var x = document.getElementById("modal");
+  document.documentElement.style.overflow = "initial";
+  x.style.display = "none";
+}
+
+function afficherNom(nom) {
+  document.getElementById("nom-du-trick").innerHTML = nom;
+}
